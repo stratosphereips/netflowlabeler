19d0537d51dec5510ff155ea5fec87298bbb6a18
Veronica Valeros <vero.valeros@gmail.com>
2023-05-21 12:14:20 +0200

    Parse 'uids' column for dhcp.log

--- zeek-files-labeler.py
+++ zeek-files-labeler.py
@@ -110,6 +110,7 @@ def define_columns(headerline, filetype):
     column_idx['detailedlabel'] = False
     column_idx['fingerprint'] = False
     column_idx['id'] = False
+    column_idx['uids'] = False
 
     try:
         if 'csv' in filetype or 'tab' in filetype:
@@ -186,6 +187,8 @@ def define_columns(headerline, filetype):
                     column_idx['label'] = nline.index(field)
                 elif 'fingerprint' in field.lower():
                     column_idx['fingerprint'] = nline.index(field)
+                elif 'uids' in field.lower():
+                    column_idx['uids'] = nline.index(field)
                 elif 'id' in field.lower():
                     column_idx['id'] = nline.index(field)
         elif 'json' in filetype:
